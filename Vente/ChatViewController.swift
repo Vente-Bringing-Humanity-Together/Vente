af7611a32b35feacd31da6b284ab5560929324f7
Nicholas Miller <nickbryanmiller@gmail.com>
2016-03-29 19:12:06 -0400

    finished the base of messaging

--- Vente/ChatViewController.swift
+++ Vente/ChatViewController.swift
@@ -14,34 +14,22 @@ class ChatViewController: UIViewController {
     
     var event: PFObject?
     var client: PubNub?
+    
+    @IBOutlet weak var myMessageTextField: UITextField!
+    @IBOutlet weak var messageTextView: UITextView!
+    
+    var eventIDString: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("in load")
         
         let configuration = PNConfiguration(publishKey: "pub-c-08213b8e-ca5d-4a44-a0e8-272d6ebbff6d", subscribeKey: "sub-c-0d695744-f5e8-11e5-8cfb-0619f8945a4f")
         // Instantiate PubNub client.
         client = PubNub.clientWithConfiguration(configuration)
         
-        client!.publish("Hello from the PubNub Swift SDK", toChannel: "my_channel",
-                           compressed: false, withCompletion: { (status) -> Void in
-                            
-                            if !status.error {
-                                
-                                // Message successfully published to specified channel.
-                                print("why")
-                            }
-                            else{
-                                print("i hate life")
-                                // Handle message publish error. Check 'category' property
-                                // to find out possible reason because of which request did fail.
-                                // Review 'errorData' property (which has PNErrorData data type) of status
-                                // object to get additional information about issue.
-                                //
-                                // Request can be resent using: status.retry()
-                            }
-        })
+        eventIDString = (event?.objectId)!
+        
+        loadMessages(eventIDString)
         
     }
 
@@ -50,9 +38,45 @@ class ChatViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    @IBAction func getMessagesPlease(sender: AnyObject) {
-        self.client?.historyForChannel("my_channel", withCompletion: { (result, status) -> Void in
+    @IBAction func sendButtonTouched(sender: AnyObject) {
+        if myMessageTextField.text != nil {
+            let me = PFUser.currentUser()!
+            let myMessage = (me["first_name"] as! String) + " " + (me["last_name"] as! String) + ": " + myMessageTextField.text!
+            sendMessageTo(myMessage, channel: eventIDString)
+        }
+    }
+    
+    func sendMessageTo(message: String, channel: String) {
+        self.client?.publish(message, toChannel: channel, compressed: false, withCompletion: { (status) -> Void in
+                                
+            if !status.error {
+                                    
+                // Message successfully published to specified channel.
+                print("successful post")
+                if (self.messageTextView.text != nil && self.messageTextView.text != "") {
+                    self.messageTextView.text = self.messageTextView.text + "\n" + message
+                }
+                else {
+                    self.messageTextView.text = message
+                }
+                
+                self.myMessageTextField.text = ""
+                
+            }
+            else {
+                print("message failed")
+                // Handle message publish error. Check 'category' property
+                // to find out possible reason because of which request did fail.
+                // Review 'errorData' property (which has PNErrorData data type) of status
+                // object to get additional information about issue.
+                //
+                // Request can be resent using: status.retry()
+            }
+        })
+    }
+    
+    func loadMessages(channel: String) {
+        self.client?.historyForChannel(channel, withCompletion: { (result, status) -> Void in
             
             if status == nil {
                 
@@ -60,7 +84,15 @@ class ChatViewController: UIViewController {
                 //   result.data.start - oldest message time stamp in response
                 //   result.data.end - newest message time stamp in response
                 //   result.data.messages - list of messages
-                print(result!.data.messages)
+                
+                for ele in result!.data.messages {
+                    if (self.messageTextView.text != nil && self.messageTextView.text != "") {
+                        self.messageTextView.text = self.messageTextView.text + "\n" + (ele as! String)
+                    }
+                    else {
+                        self.messageTextView.text = ele as! String
+                    }
+                }
             }
             else {
                 
@@ -73,6 +105,5 @@ class ChatViewController: UIViewController {
             }
         })
     }
-    
 
 }
