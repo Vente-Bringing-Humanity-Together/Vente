3eea5151c139365c9c3e774eb70ba4b0c416aa69
Nicholas Miller <nickbryanmiller@gmail.com>
2016-03-31 14:35:07 -0400

    Added past event

---
+++ Vente/PeopleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleTableViewCell.swift
+//  Vente
+//
+//  Created by Nicholas Miller on 3/31/16.
+//  Copyright © 2016 nickbryanmiller. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Vente/ProfileViewController.swift
+++ Vente/ProfileViewController.swift
@@ -13,7 +13,7 @@ import FBSDKLoginKit
 
 let userDidLogoutNotification = "userDidLogoutNotification"
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var profileImageView: UIImageView!
@@ -21,34 +21,125 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
     
+    @IBOutlet weak var optionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var events: [PFObject]!
+    var followingArray: [String]! = []
+    var followerArray: [String]! = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        doDatabaseQuery()
 
-        getUserData();
     }
     
     override func viewWillAppear(animated: Bool) {
-        
-        let user = PFUser.currentUser()
-        
-        if (user?["profile_image"] != nil) {
-            let userImageFile = user?["profile_image"] as! PFFile
-            userImageFile.getDataInBackgroundWithBlock({ (imageData: NSData?, error: NSError?) -> Void in
-                if let error = error {
-                    print(error.localizedDescription)
-                }
-                else {
-                    let image = UIImage(data: imageData!)
-                    self.profileImageView.image = image
-                }
-            })
-        }
+        super.viewWillAppear(true)
+        getUserData();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (optionSegmentedControl.selectedSegmentIndex == 0) {
+            if (self.events != nil) {
+                return self.events!.count
+            }
+            else {
+                return 0
+            }
+        }
+        else if (optionSegmentedControl.selectedSegmentIndex == 1) {
+            if (self.followingArray != nil && self.followingArray != []) {
+                return self.followingArray!.count
+            }
+            else {
+                return 0
+            }
+        }
+        else if (optionSegmentedControl.selectedSegmentIndex == 2) {
+            if (self.followerArray != nil && self.followerArray != []) {
+                return self.followerArray!.count
+            }
+            else {
+                return 0
+            }
+        }
+        else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if (optionSegmentedControl.selectedSegmentIndex == 0) {
+            let cell = tableView.dequeueReusableCellWithIdentifier("MyEventsTableViewCell") as! MyEventsTableViewCell
+            
+            cell.Event = events[indexPath.row]
+            
+            if (events?[indexPath.row]["event_image"] != nil) {
+                let userImageFile = events?[indexPath.row]["event_image"] as! PFFile
+                userImageFile.getDataInBackgroundWithBlock({ (imageData: NSData?, error: NSError?) -> Void in
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                    else {
+                        if(imageData != nil){
+                            let image = UIImage(data: imageData!)
+                            cell.eventImageView.image = image
+                        }
+                    }
+                })
+            }
+            
+            return cell
+        }
+        else if (optionSegmentedControl.selectedSegmentIndex == 1) {
+            return UITableViewCell()
+        }
+        else if (optionSegmentedControl.selectedSegmentIndex == 2) {
+            return UITableViewCell()
+        }
+        else {
+            return UITableViewCell()
+        }
+        
+    }
+    
+    func doDatabaseQuery() {
+        let userId = PFUser.currentUser()?.objectId
+        let query = PFQuery(className: "Events")
+        query.limit = 20
+        query.whereKey("attendee_list", equalTo: userId!)
+        query.orderByDescending("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock { (results: [PFObject]?, error: NSError?) -> Void in
+            if let error = error {
+                print("Error: \(error)")
+            } else {
+                if let results = results {
+                    print("Successfully retrieved \(results.count) ventes")
+                    self.events = results
+                    self.tableView.reloadData()
+                } else {
+                    print("No results returned")
+                }
+            }
+        }
+
     }
     
     @IBAction func editButtonTouched(sender: AnyObject) {
@@ -101,6 +192,10 @@ class ProfileViewController: UIViewController {
             })
         }
         
+    }
+    
+    @IBAction func optionSegmentedControlChanged(sender: AnyObject) {
+        tableView.reloadData()
     }
     
 
